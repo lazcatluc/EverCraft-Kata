2d805cb6bd72560fb71caf21faf21663c8cf2eec
Catalin Lazar <catalin.lazar@endava.com>
2017-08-29 20:00:23 +0300

    Add get and set for all alignments

--- java/src/test/java/mmocharacter/AlignmentTest.java
+++ java/src/test/java/mmocharacter/AlignmentTest.java
@@ -6,10 +6,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class AlignmentTest {
     @Test
-    public void canSetAlignment() throws Exception {
+    public void canBecomeGood() throws Exception {
         MmoCharacter character = new MmoCharacter();
         character.setAlignment(Alignment.GOOD);
 
         assertThat(character.getAlignment()).isEqualTo(Alignment.GOOD);
+    }
+
+    @Test
+    public void canBecomeNeutral() throws Exception {
+        MmoCharacter character = new MmoCharacter();
+        character.setAlignment(Alignment.NEUTRAL);
+
+        assertThat(character.getAlignment()).isEqualTo(Alignment.NEUTRAL);
+    }
+
+    @Test
+    public void canBecomeEvil() throws Exception {
+        MmoCharacter character = new MmoCharacter();
+        character.setAlignment(Alignment.EVIL);
+
+        assertThat(character.getAlignment()).isEqualTo(Alignment.EVIL);
     }
 }
